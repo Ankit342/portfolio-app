57896062c5a33f7794bfd2299611a768d4e7031c
Ankit342 <75938305+Ankit342@users.noreply.github.com>
2024-02-24 22:56:16 +0530

    commit

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,49 +1,72 @@
 import styles from "./Contacts.module.css";
-import {useRef} from 'react';
+import {useState} from 'react';
 
 export const Contacts = () => {
-    const nameRef = useRef(null);
-    const emRef = useRef(null);
-    const meRef = useRef(null);
-    const handleSubmit = event =>{
-        event.preventDefault();
-        nameRef.current.value = '';
-        emRef.current.value = '';
-        meRef.current.value = '';
+    const [data, setData] = useState({
+        name:"",
+        email:"",
+        message:"",
+    });
+    const {name,email,message}=data;
+    const handleChange = (e)=>{
+        setData({...data,[e.target.name]:e.target.value});
     };
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        try{
+            const response = await fetch(
+                "https://v1.nocodeapi.com/djack/google_sheets/zgzXvBciwvYdgjqK?tabId=Sheet1",
+                {
+                    method:"POST",
+                    headers:{
+                        'Content-Type': 'application/json'
+                    },
+                    body:JSON.stringify([[name,email,message, new Date().toLocaleString()]])
+                }
+            );
+            await response.json();
+            setData({...data, name : "",email:"",message:""});
+        }catch(err){
+            console.log(err);
+        }
+    }
     return (
         <form
             className={styles.wrapper}
-            target="_blank"
-            action="https://formsubmit.co/ankits42057@gmail.com"
-            method="POST"
+            // id="form"
+            // target="_blank"
+            // action="https://formsubmit.co/ankits42057@gmail.com"
+            // method="POST"
             onSubmit={handleSubmit}
         >
             <div className={styles.inputs}>
                 <input
-                    ref={nameRef}
                     className={styles.input}
                     type="text"
                     name="name"
                     placeholder="Your name"
+                    value={name}
+                    onChange={handleChange}
                     required
                 />
                 <input
-                    ref={emRef}
                     className={styles.input}
                     type="email"
                     name="email"
                     placeholder="Your email"
+                    value={email}
+                    onChange={handleChange}
                     required
                 />
             </div>
             <div className={styles.textareas}>
                 <textarea
-                    ref={meRef}
                     className={styles.textarea}
                     type="text"
                     name="message"
                     placeholder="Your message"
+                    value={message}
+                    onChange={handleChange}
                     required
                 />
                 <button className={styles.send} type="submit">
